3fd6c65455124e5168a7173126027068369ca3ec
Dominik Suwala <suwalad@faf.cuni.cz>
2025-03-31 15:41:39 +0200

    fixed suggestions api and added recommendation call

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, jsonify, send_from_directory, request
 from flask_cors import CORS
 import requests
+import logging
 
 app = Flask(__name__, static_folder='static')
 CORS(app)
 PREDICTION_ENGINE_URL = "http://localhost:8000"
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 
 @app.route('/')
@@ -14,14 +18,49 @@ def serve_index():
 
 @app.route('/antirecommender', methods=['POST'])
 def antirecommender():
-    # Mock antirecommender endpoint
-    return jsonify({
-        'recommendations': [
-            'If you like coffee, try staying awake all night',
-            'If you enjoy reading, try reading backwards',
-            'If you like music, try listening to everything at 0.5x speed'
-        ]
-    })
+
+    data = request.json
+    query = data.get("query")
+
+    json = {"movie_title": query}
+
+    if data.get("year"):
+        year = data.get("year")
+        json.update({"year": year})
+
+    # Make request to the recommender API
+    response = requests.post(
+        f"{PREDICTION_ENGINE_URL}/recommend",
+        json=json
+    )
+
+    logger.info(f"Response: {response.json()}")
+
+    if "error" in response.json().keys():
+        possible_matches = [f"Title: {result[0]}, year: {int(result[1])}" for result in response.json()["possible_matches"]]
+
+        result = jsonify({
+            "query": query,
+            "possible_matches": possible_matches[:3]
+        })
+
+        return result
+    else:
+
+        recommendations = [f"Title: {result['standardized_title']}, year: {int(result['year'])}, rating: {result['rating']:.2f}" for result in response.json()["recommendations"]]
+        result = jsonify({
+            "query": query,
+            "results": recommendations
+        })
+
+        return result
+    # return jsonify({
+    #     'recommendations': [
+    #         'If you like coffee, try staying awake all night',
+    #         'If you enjoy reading, try reading backwards',
+    #         'If you like music, try listening to everything at 0.5x speed'
+    #     ]
+    # })
 
 
 @app.route('/taste', methods=['POST'])
@@ -40,15 +79,13 @@ def taste():
 @app.route('/antirecommender/search-suggestions', methods=['GET'])
 def get_suggestions():
     query = request.args.get('query', '').lower()
-    print(query)
+    logger.info(f"Received search suggestions request for query: {query}")
 
     response = requests.get(f"{PREDICTION_ENGINE_URL}/search-suggestions", params={'query': query})
     response_json = response.json()
-    print(response_json)
-    matching_movies = [
-        movie for movie in response_json.suggestions
-        if query in movie.lower()][:10]  # limit to 10 suggestions
-    return jsonify(matching_movies)
+    logger.info(f"Suggestions: {response_json}")
+    suggestions = response_json['suggestions']
+    return jsonify(suggestions)
 
 
 if __name__ == '__main__':
