1ead1972a62953996cfbc24a5ace920f0e2c880d
Dominik Suwala <suwalad@faf.cuni.cz>
2025-04-11 11:58:41 +0200

    debug terraform authentication

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,39 +5,39 @@ provider "google" {
 
 
 module "modules-iam-service-account" {
-  source = "./modules/iam-service-account"
-  project_id = var.project_id
+  source                          = "./modules/iam-service-account"
+  project_id                      = var.project_id
   antirecommender_service_account = var.antirecommender_service_account
-  homepage_service_account = var.homepage_service_account
-  github_deploy_service_account = var.github_deploy_service_account
+  homepage_service_account        = var.homepage_service_account
+  github_deploy_service_account   = var.github_deploy_service_account
 }
 
 
 module "modules-services" {
-  source = "./modules/services"
-  project_id = var.project_id
+  source         = "./modules/services"
+  project_id     = var.project_id
   project_number = var.project_number
 }
 
 
 module "modules-cloud-run" {
-  source = "./modules/cloud-run"
-  project_id = var.project_id
-  region = var.region
+  source                          = "./modules/cloud-run"
+  project_id                      = var.project_id
+  region                          = var.region
   antirecommender_service_account = var.antirecommender_service_account
-  homepage_service_account = var.homepage_service_account
-  project_number = var.project_number
+  homepage_service_account        = var.homepage_service_account
+  project_number                  = var.project_number
 }
 
 
 module "modules-artifact-registry" {
-  source = "./modules/artifact-registry"
+  source     = "./modules/artifact-registry"
   project_id = var.project_id
 }
 
 
 module "modules-storage" {
-  source = "./modules/storage"
-  project_id = var.project_id
+  source      = "./modules/storage"
+  project_id  = var.project_id
   bucket_name = var.bucket_name
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,25 +13,25 @@ variable "antirecommender_service_account" {
   description = "Service account name for the antirecommender Cloud Run service"
   type        = string
   sensitive   = true
-} 
+}
 
 variable "homepage_service_account" {
   description = "Service account name for the homepage Cloud Run service"
   type        = string
   sensitive   = true
-} 
+}
 
 variable "project_number" {
   description = "The GCP project number"
   type        = string
   sensitive   = true
-} 
+}
 
 variable "github_deploy_service_account" {
   description = "Service account name for the github deploy service"
   type        = string
   sensitive   = true
-} 
+}
 
 variable "bucket_name" {
   description = "The GCP bucket name"
