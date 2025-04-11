8a610197f9a2be4b71c17e07fc02ddf02d1a60c9
Dominik Suwala <suwalad@faf.cuni.cz>
2025-04-11 11:45:26 +0200

    updated terraform authentication

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,7 @@
 variable "project_id" {
   description = "The GCP project ID"
   type        = string
+  sensitive   = true
 }
 
 variable "region" {
@@ -11,21 +12,25 @@ variable "region" {
 variable "antirecommender_service_account" {
   description = "Service account name for the antirecommender Cloud Run service"
   type        = string
+  sensitive   = true
 } 
 
 variable "homepage_service_account" {
   description = "Service account name for the homepage Cloud Run service"
   type        = string
+  sensitive   = true
 } 
 
 variable "project_number" {
   description = "The GCP project number"
   type        = string
+  sensitive   = true
 } 
 
 variable "github_deploy_service_account" {
   description = "Service account name for the github deploy service"
   type        = string
+  sensitive   = true
 } 
 
 variable "bucket_name" {
